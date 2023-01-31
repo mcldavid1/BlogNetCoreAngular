791a32a312749075b1736e1c5411ca09769b525e
Michael David <michael.david@redriver.com>
2023-01-31 15:16:58 -0800

    check

--- BlogAngular/src/app/app.module.ts
+++ BlogAngular/src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SummaryPipe } from './pipes/summary.pipe';
+import { BlogComponent } from './components/blog-components/blog/blog.component';
+import { BlogCardComponent } from './components/blog-components/blog-card/blog-card.component';
+import { CommentComponent } from './components/comment-components/comment/comment.component';
+import { CommentsComponent } from './components/comment-components/comments/comments.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SummaryPipe
+    SummaryPipe,
+    BlogComponent,
+    BlogCardComponent,
+    CommentComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule,
